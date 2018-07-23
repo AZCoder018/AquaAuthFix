13d6b6a8cfc89f190fe273ac4cc5c3f55592e2ec
agippos <agippos@gmail.com>
2018-07-23 04:01:44 -0700

    get rid of uglify?

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -3,7 +3,7 @@ const merge = require('webpack-merge');
 
 const helpers = require('./helpers');
 const commonConfig = require('./webpack.common');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+// const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = merge(commonConfig, {
   mode: 'production',
@@ -11,24 +11,8 @@ module.exports = merge(commonConfig, {
   output: {
     filename: 'js/[name].[hash].js',
     chunkFilename: '[id].[hash].chunk.js'
-  },
+  }
 
-  optimization: {
-      minimizer: [
-          //https://github.com/mishoo/UglifyJS2/tree/harmony
-          new UglifyJsPlugin({
-              uglifyOptions: {
-                  output: {
-                      comments: false
-                  },
-                  minify: {},
-                  compress: {
-                      booleans: false,
-                      //...
-                  }
-              }
-          }),
-      ]
-    },
+
   
 });
